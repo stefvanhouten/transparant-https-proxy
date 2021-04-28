0193e6d7f86c3ea9a845be1071dd92b3277bca91
Stef van Houten <stefvanhouten@gmail.com>
2021-04-28 15:15:59 +0200

    Small refractor in parser. Now also supports images.

--- html-parser/parser.py
+++ html-parser/parser.py
@@ -5,55 +5,79 @@ import os
 
 LOOKUP = {
   'p': 'text',
-  'div':'content',
+  'div':'container',
+  'body': 'content',
 }
+
+EXCLUDE = (
+  'script',
+  'noscript',
+  'html',
+  )
+
 class HTMLParser:
   def __init__(self, file):
-    self.html = ""
-    self.xml = ""
-    self._formatted_xml_string = ""
-
-    self._file = file
     TreeBuilder = html5lib.getTreeBuilder("lxml")
     self._parser = html5lib.HTMLParser(tree=TreeBuilder)
     self._tree_walker = html5lib.getTreeWalker('lxml')
-    self._dom_tree = self._create_dom_tree()
+
+    self._file = file
+    self._formatted_xml_string = ""
+    self.html, self._dom_tree = self._create_dom_tree()
     self._stream = self._tree_walker(self._dom_tree)
-    self._convert_html_to_xml()
+    self.xml = self._convert_html_to_xml()
     self._pretty_xml()
 
   def _create_dom_tree(self):
     with open(self._file, 'rb') as f:
-      self.html = f.read()
-      return self._parser.parse(self.html)
+      html = f.read()
+      dom_tree = self._parser.parse(html)
+      return html, dom_tree
 
   def _convert_html_to_xml(self):
-    self.xml += '<?xml version="1.0"?><data>'
+    INTERESTED_CHARACTERS = ('Characters', 'SpecialCharacters',)
+
+    converted_html = ""
     for item in self._stream:
       if 'name' in item:
-        self._build_tag(item)
-      else:
-        if item['type'] in ('Characters', 'SpecialCharacters',):
-          self.xml += bleach.clean(item['data'])
-    self.xml += '</data>'
+        tag = self._build_tag(item)
+        if tag is not None:
+          converted_html += tag
+        continue
+      if item['type'] in INTERESTED_CHARACTERS:
+        converted_html += bleach.clean(item['data'])
+    return f'<?xml version="1.0"?><data>{converted_html}</data>'
 
   def _build_tag(self, tag: dict) -> None:
-    if tag['type'] == 'StartTag':
-      self._build_start_tag(tag)
-    elif tag['type'] == 'EndTag':
-      self._build_end_tag(tag)
+    type = tag['type']
+    if type == 'StartTag':
+      return self._build_start_tag(tag)
+
+    if type == 'EndTag':
+      return self._build_end_tag(tag)
+
+    if type == 'EmptyTag':
+      if tag['name'] == 'img':
+        return self._build_img_tag(tag)
 
   def _build_start_tag(self, tag):
     converted_tag = LOOKUP.get(tag['name'], tag['name'])
-    if converted_tag in ('script', 'noscript',):
+    if converted_tag in EXCLUDE:
       return
-    self.xml += f'<{converted_tag}>'
+    return f'<{converted_tag}>'
 
   def _build_end_tag(self, tag: dict) -> None:
     converted_tag = LOOKUP.get(tag['name'], tag['name'])
-    if converted_tag in ('script', 'noscript',):
+    if converted_tag in EXCLUDE:
       return
-    self.xml += f'</{converted_tag}>'
+    return f'</{converted_tag}>'
+
+  def _build_img_tag(self, tag: dict):
+    new_tag = ""
+    for tag, value in tag['data'].items():
+      _, tag_name = tag
+      new_tag += f'<{tag_name}>{value}</{tag_name}>'
+    return f'<img>{new_tag}</img>'
 
   def _pretty_xml(self):
     root = etree.fromstring(self.xml)
