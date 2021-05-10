b60f59292c122a88eddc6a0745c9f5c0b4292061
SDanDyS <gameking23@hotmail.com>
2021-05-10 10:39:42 +0200

    hh

--- proxyparser/test.py
+++ proxyparser/test.py
@@ -5,6 +5,6 @@ from mitmproxy import http
 def request(flow: http.HTTPFlow) -> None:
 	flow.response = http.HTTPResponse.make(
 		200,  # (optional) status code
-		flow.request.text,  # (optional) content
+		flow.request.text,  # (optional) content ##DUMP THIS IN A FILE TO SEE WHAT KIND OF REQUEST YOU GET. IT DOES NOT RETURN RAW HTML BUT JSON 
 		{"Content-Type": "text/html"}  # (optional) headers
 	)
