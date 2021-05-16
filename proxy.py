7dcaf4005277cb8204eb302ff55e85ee8b9879c2
stefvanhouten <stefvanhouten@gmail.com>
2021-05-16 11:11:04 +0200

    Proxy now uses basic flask API to get EXCLUDE config.

--- proxy.py
+++ proxy.py
@@ -1,23 +1,20 @@
+import requests
+import json
+
 from mitmproxy import ctx
 from htmlparser.parser import HTMLParser
-
-EXCLUDE = (
-  'script',
-  'noscript',
-  'html',
-  'style',
-  'doctype',
-  'meta',
-  )
-
-parser = HTMLParser(EXCLUDE)
 class Parser:
+  def request(self, flow):
+    res = requests.get('http://127.0.0.1:5000/proxy')
+    data = json.loads(res.content)
+    self.parser = HTMLParser(data.get('exclude', ['html', 'script', 'style']))
+
   def response(self, flow):
     if not 'html' in flow.response.headers['Content-Type']:
       return
 
     flow.response.headers['Content-Type'] = 'application/xml'
-    flow.response.text = parser.parse(flow.response.text)
+    flow.response.text = self.parser.parse(flow.response.text)
 
 addons = [
     Parser()
