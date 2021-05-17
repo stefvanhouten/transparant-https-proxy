a8e5d7a80a3f04516b4de2c869f0184b67a0518a
SDanDyS <gameking23@hotmail.com>
2021-05-17 18:14:37 +0200

    install normalizer via pipy.

--- proxy.py
+++ proxy.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+from charset_normalizer import CharsetNormalizerMatches as CnM
 from mitmproxy import ctx
 from htmlparser.parser import HTMLParser
 class Parser:
@@ -14,7 +15,22 @@ class Parser:
       return
 
     flow.response.headers['Content-Type'] = 'application/xml'
-    flow.response.text = self.parser.parse(flow.response.text)
+
+    utf8String = charset_normalizer(flow.response.content)
+
+    flow.response.text = self.parser.parse(utf8String)
+
+  def charset_normalizer(self, byte_string):
+    result = CnM.from_bytes(
+      byte_string,
+      preemptive_behaviour=True  # Determine if we should look into my_byte_str (ASCII-Mode) for pre-defined encoding
+    ).best().first() #KEEP ONLY THE MATCHES WITH THE LOWEST RATIO OF CHAOS - SELECT THE FIRST MATCH AVAILABLE
+
+    if len(result) > 0:
+      #ENCODE TO UTF-8
+      utf8String = str(result)
+      return utf8String
+
 
 addons = [
     Parser()
