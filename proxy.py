bdddd1d97de0d3400bb36f45bcedee8bb78c5712
SDanDyS <gameking23@hotmail.com>
2021-05-26 17:47:57 +0200

    implement get_content

--- proxy.py
+++ proxy.py
@@ -14,13 +14,14 @@ class Parser:
     if not 'html' in flow.response.headers['Content-Type']:
       return
 
-    utf8String = self.charset_normalizer(flow.response.raw_content)
+    utf8String = self.charset_normalizer(flow.response.get_content(False))
     flow.response.headers['Content-Type'] = 'application/xml'
     flow.response.text = self.parser.parse(utf8String)
 
   def charset_normalizer(self, byte_string):
     result = CnM.from_bytes(
-      str(byte_string),
+      byte_string,
+      threshold = 100, #threshold is set to high, to prevent filtering, change to desired threshold when deploying
       preemptive_behaviour=True,  # Determine if we should look into my_byte_str (ASCII-Mode) for pre-defined encoding
       explain=True  # Print on screen what is happening when searching for a match (FOR DEBUGGING PURPOSES)
     ).best().first() #KEEP ONLY THE MATCHES WITH THE LOWEST RATIO OF CHAOS - SELECT THE FIRST MATCH AVAILABLE
