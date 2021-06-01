ce7eccf1585d86c0d4a119403182ed8cd1261289
stefvanhouten <stefvanhouten@gmail.com>
2021-06-01 13:51:14 +0200

    Add extra routes to support reading and creating configs

--- api/routes/proxy.py
+++ api/routes/proxy.py
@@ -1,11 +1,14 @@
-from http import HTTPStatus
 from functools import wraps
-from marshmallow.exceptions import ValidationError
+from http import HTTPStatus
 
 from flask import Blueprint, jsonify, request
-from api.routes.schema import GetConfigSchema, CreateConfigSchema
+from marshmallow.exceptions import ValidationError
+
+from api.models import Configurations
+from api.routes.schema import CreateConfigSchema, GetConfigSchema
 
 bp = Blueprint("proxy", __name__, url_prefix="/proxy")
+
 
 def error_wrapper(func):
     @wraps(func)
@@ -13,19 +16,25 @@ def error_wrapper(func):
         try:
             return func(*args, **kwargs)
         except ValidationError as err:
-            return jsonify({
-                "error": True,
-                "errors": err.messages,
-                "status": HTTPStatus.BAD_REQUEST
-            })
+            return jsonify(
+                {
+                    "error": True,
+                    "errors": err.messages,
+                    "status": HTTPStatus.BAD_REQUEST,
+                }
+            )
         except Exception as err:
             print(err)
-            return jsonify({
-                "error": True,
-                "errors": [],
-                "status": HTTPStatus.INTERNAL_SERVER_ERROR
-            })
+            return jsonify(
+                {
+                    "error": True,
+                    "errors": [],
+                    "status": HTTPStatus.INTERNAL_SERVER_ERROR,
+                }
+            )
+
     return wrapper
+
 
 @bp.route("/")
 def hello():
@@ -41,21 +50,44 @@ def hello():
     )
 
 
-@bp.route("/get_schema/<ip>", methods=["GET"])
+@bp.route("/get_schema/<ip>/<name>", methods=["GET"])
 @error_wrapper
-def get_schema(ip):
-    response = {}
-    ip = GetConfigSchema().load({"ip": ip})
-    return jsonify(
-        {
-            "exclude": [
-                "script",
-                "noscript",
-                "html",
-                "style",
-            ]
-        }
-    )
+def get_schema(ip, name):
+    response = {
+        "error": False,
+        "errors": [],
+        "config": None,
+        "status": HTTPStatus.OK,
+    }
+    sanitzed_data = GetConfigSchema().load({"ip": ip, "name": name})
+
+    config = Configurations.query.filter_by(
+        ip=sanitzed_data["ip"], name=sanitzed_data["name"]
+    ).first()
+
+    data = CreateConfigSchema(exclude=["exclude_elements"]).dump(config)
+    exclude_elements = []
+
+    if not config:
+        return jsonify(
+            {
+                "error": True,
+                "errors": [
+                    f"Config with combination ip: '{ip}' and name: '{name}' not found"
+                ],
+                "status": HTTPStatus.NOT_FOUND,
+            }
+        )
+
+    # Quick and dirty fix
+    if len(config.exclude_elements) != 0:
+        items = config.exclude_elements.split(",")
+        for element in items:
+            exclude_elements.append(element)
+        data["exclude_elements"] = exclude_elements
+
+    response["config"] = data
+    return response
 
 
 @bp.route("/create_schema", methods=["POST"])
@@ -73,5 +105,3 @@ def create_schema():
             ]
         }
     )
-
-
