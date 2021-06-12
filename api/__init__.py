ba451cc21704e3463a1dda8c86aee41a1d998d25
stefvanhouten <stefvanhouten@gmail.com>
2021-06-12 16:32:28 +0200

    Move DB to be initialized in same file as app

--- api/__init__.py
+++ api/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 
-from .models import db
 
 app = Flask(__name__)
-ma = Marshmallow(app)
 app.config.from_pyfile("config.py", silent=True)
-db.init_app(app)
+
+ma = Marshmallow(app)
+db = SQLAlchemy(app)
 
 
 @app.cli.command("init-db")
 def init_db():
     db.create_all()
 
+@app.cli.command("remove-db")
+def remove_db():
+    db.drop_all()
 
 from .routes import proxy
 
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,14 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.schema import UniqueConstraint
-
-db = SQLAlchemy()
-
+from api import db
 
 class Configurations(db.Model):
     __tablename__ = "configurations"
+    __table_args__ = (db.UniqueConstraint("ip", "name", name="uix_1"),)
+
     ip = db.Column(db.String(255), primary_key=True, nullable=False)
     name = db.Column(db.String(255), primary_key=True, nullable=False)
     block_iso = db.Column(db.Boolean)
     exclude_elements = db.Column(db.String(255))
-
-    UniqueConstraint("ip", "name", name="uix_1")
